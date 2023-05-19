cca909f64685afc8a558a3c2041d48eec0861dcb
22_yourspring <skywooya@icloud.com>
2023-05-19 09:44:09 +0900

    add - Homework_0518

--- Homework_JHW/Homework_0518/Homework_0518.cpp
+++ Homework_JHW/Homework_0518/Homework_0518.cpp
@@ -1,20 +1,92 @@
-﻿// Homework_0518.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include<iostream>
 
-#include <iostream>
+using namespace std;
 
-int main()
+int StringCount(char* _Arr)
 {
-    std::cout << "Hello World!\n";
+	if (_Arr == nullptr)
+	{
+		return 0;
+	}
+	/*
+		_Arr가 nullptr인 경우, 아무 문자 혹은 문자열을 가지고 있지 않음으로 0을 리턴한다.
+	*/
+
+	int Answer = 0;
+	while (*_Arr != '\0')
+	{
+		if (*_Arr != ' ')
+		{
+			++Answer;
+		}
+		++_Arr;
+	}
+	/*
+		1. _Arr가 가르키고 있는 값이 0 혹은 '\0'이 아닐 경우 false임으로 while문을 종료한다.
+		2. 만약 _Arr가 가르키고 있는 값이 ' '(띄어쓰기)가 아닐경우 Answer의 값을 증가시킨다.
+		3. 증가된 Answer값을 리턴한다.
+	*/
+
+	return Answer;
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+void ChangeCh(char* _Arr, char _PrevCh, char _NextCh)
+{
+	if (_Arr == nullptr)
+	{
+		return;
+	}
+	/*
+		_Arr가 아무주소도 가르키고 있지 않은 경우, 함수를 종료한다.
+	*/
+	while (*_Arr != '\0')
+	{
+		if (*_Arr == _PrevCh)
+		{
+			*_Arr = _NextCh;
+		}
+		++_Arr;
+	}
+	/*	while문 자가해석
+		1. 입력 받은 _Arr가 '\0'이 되면 false임으로 while문 종료.
+		2. 만약, _Arr가 가르키고 있는 값이 _PrevCh라면,
+		3. _Arr가 가르키고 있는 값을 _NextCh로 변환한다.
+		   (주소에 있는 값을 직접 바꿔줌으로 ChangeCh함수가 끝나도 변환이 된다.)
+		4. _Arr의 자료형은 Char* 임으로, ++연산을 할 시 sizeof(char)값 만큼 주소를 증가시킨다.
+		   (_Arr의 주소가 100번지라고 가정하면, ++_Arr를 할 경우, while문이 끝날때까지 주소값을 1씩 증가시킨다. )
+	*/
+}
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+void ChangeCh2(char* _Arr, char _PrevCh, char _NextCh)
+{
+	if (_Arr == nullptr)
+	{
+		return;
+	}
+
+	for (; *_Arr != '\0'; ++_Arr)
+	{
+		if (*_Arr == _PrevCh)
+		{
+			*_Arr = _NextCh;
+		}
+	}
+	/*
+	* while문을 for문으로 바꿔보았다.
+	*/
+}
+
+int main(void)
+{
+	char str[100] = "aaa bbb ccc";
+
+	int Count = StringCount(str);
+	ChangeCh(str, 'c', 'd');
+
+	cout << Count << " " << str << endl;
+	/*
+	* Expected Result.
+	* StringCount = 9.
+	* ChageCh = aaa bbb ddd'
+	*/
+}
