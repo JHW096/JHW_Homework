39489aa4155e9db46333c02287ff0ddc45070eda
Jeon <dbs_dmswl92@naver.com>
2023-05-22 17:22:35 +0900

    Submit - Homework_0522

--- Homework_JHW/Homework_0522/Homework_0522.cpp
+++ Homework_JHW/Homework_0522/Homework_0522.cpp
@@ -1,31 +1,57 @@
-﻿#include <iostream>
-#include <conio.h> // 키를 누를때까지 기다리는 함수
+﻿#include<iostream>
+#include<conio.h>
 
-// 숙제 1 -- 문자의 오른쪽끝을 맞춰오세요. 이름은 알파뱃만
-// 오른쪽끝 -------
+/*
+    Homework_0522
+    1. 문자의 오른쪽 끝을 맞춰오세요. 이름은 알파벳만, 글자수 40제한.
+    2. 실시간으로 만들어오세요. 데미지 메세지가 나오는 순간 체력이 곧바로 감소되게끔.
+    3. 누군가 죽었다면(Hp가 0이 된다면) 종료하세요.
 
-// 숙제 2 -- 실시간으로 만들어오세요.
-// 데미지 메세지가 나오는순간 체력이 곧바로 감소되게 만들어라.
-// 화면 지우는 함수 화면에 출력하는 함수를 잘 생각해보세요.
-// 그냥 무식하게 생각해라.
+    몬스터와 플레이어가 서로 한 대씩 때리면서 한 명이 죽으면 끝납니다.
+    그래픽으로 표현할 수 없으니 출력으로 표현합니다.
+*/
 
-// 숙제 3 -- 누군가 죽었다. 누군가의 Hp가 0이 된순간 종료.
+int GetStringSize(const char* const _Name)
+{
+    if (_Name == nullptr)
+    {
+        return 0;
+    }
+    int Count = 0;
 
-
-// 몬스터와 플레이어가 서로 한대씩 때리면서
-// 한명이 죽으면 끝납니다.
-// 지금 그걸 그래픽으로 표현할 방법이 없으니까.
-// 글자로 표현하겠습니다.
+    while (_Name[Count] != '\0')
+    {
+        ++Count;
+    }
+    return Count;
+}
 
 void StatusRender(const char* const _Name, int _Att, int _Hp)
 {
-    // 40개로 맞춰오세요.
+    if (_Name == nullptr)
+    {
+        printf_s("이름을 만들어 주세요.\n");
+        return;
+    }
 
-    // 줄바꿈이 안되고 있습니다.
-    printf_s("%s--------------------------\n", _Name);
+    int _NameSize = GetStringSize(_Name);
+    if (_NameSize > 40)
+    {
+        return;
+    }
+
+    printf_s("%s", _Name);
+    for (int i = _NameSize; i < 40; ++i)
+    {
+        printf_s("-");
+    }
+    printf_s("\n");
+
     printf_s("공격력 : %d\n", _Att);
     printf_s("체  력 : %d\n", _Hp);
-    printf_s("--------------------------\n");
+
+    printf_s("----------------------------------------\n");
+
 }
 
 void DamagePrint(const char* const _AttName, const char* const _DefName, int _Att)
@@ -34,92 +60,70 @@ void DamagePrint(const char* const _AttName, const char* const _DefName, int _At
     printf_s("%s는 %d의 데미지를 입었습니다.\n", _DefName, _Att);
 }
 
+
 void Damage(int& _Hp, int _Att)
 {
     _Hp -= _Att;
 }
 
-// 내일꼭 break continue
-
-//1.
-
-int GetNameCount(const char* const _Name)
-{
-    int Count = 0;
-
-    while (*_Name != '\0')
-    {
-        ++Count;
-    }
-
-    return Count;
-}
-
-void MyStatusRender(const char* const _Name, int _Att, int _Hp)
-{
-    if (_Name == nullptr)
-    {
-        return;
-    }
-
-    if (GetNameCount(_Name) > 40)
-    {
-        return;
-    }
-    // 0  1  2  3
-    //[N][a][m][e]...[\0]
-
-    std::cout << _Name << std::endl;
-    /*for (int i = GetNameCount(_Name); i < 40; ++i) 
-    {
-        printf_s("-");
-    }
-    printf_s("\0");*/
-
-}
-
 int main()
 {
     int PlayerHp = 200;
-    int PlayerAtt = 100;
-    char PlayerName[40] = "fasdhjkfasdhjkfasdhjfklasdhjkfasdhjfk";
+    int PlayerAtt = 20;
+    char PlayerName[40] = "PlayerName";
 
     int MonsterHp = 100;
     int MonsterAtt = 10;
-    char MonsterName[40] = "Monster";
+    char MonsterName[40] = "MonsterName";
+
+    //초기 화면 출력
+    StatusRender(PlayerName, PlayerAtt, PlayerHp);
+    StatusRender(MonsterName, MonsterAtt, MonsterHp);
+
+    while (true)
+    {
+
+        _getch();
+
+        Damage(MonsterHp, PlayerAtt);
+
+        system("cls");
+
+        StatusRender(PlayerName, PlayerAtt, PlayerHp);
+        StatusRender(MonsterName, MonsterAtt, MonsterHp);
+        DamagePrint(PlayerName, MonsterName, PlayerAtt);
+
+        if (PlayerHp <= 0 || MonsterHp <= 0)
+        {
+            printf_s("게임을 종료합니다.\n");
+            return 0;
+        }
+
+        /*
+            1. 키를 누른다.
+            2. 데미지를 먼저 계산한다.
+            3. 이전 화면을 지운다.
+            4. 계산된 데미지와 데미지 정보를 출력한다.
+            Hp를 확인하여 프로그램(게임)을 종료한다.
+        */
+
+        _getch();
+
+        Damage(PlayerHp, MonsterAtt);
+
+        system("cls");
+
+        StatusRender(PlayerName, PlayerAtt, PlayerHp);
+        StatusRender(MonsterName, MonsterAtt, MonsterHp);
+        DamagePrint(MonsterName, PlayerName, MonsterAtt);
+
+        if (PlayerHp <= 0 || MonsterHp <= 0)
+        {
+            printf_s("게임을 종료합니다.\n");
+            return 0;
+        }
+
+    }
 
 
-    MyStatusRender(MonsterName, 10, 20);
-
-    //while (true)
-    //{
-    //    // 콘솔창을 모조리 지우는 함수
-    //    system("cls");
-
-    //    // 한번에 함수로 만든것이고
-    //    StatusRender(PlayerName, PlayerAtt, PlayerHp);
-    //    StatusRender(MonsterName, MonsterAtt, MonsterHp);
-
-    //    _getch();
-
-    //    // 아무키나 입력할때까지 기다리는 함수
-
-    //    DamagePrint(PlayerName, MonsterName, PlayerAtt);
-    //    Damage(MonsterHp, PlayerAtt);
-    //    // MonsterHp -= PlayerAtt;
-
-    //    _getch();
-
-    //    DamagePrint(MonsterName, PlayerName, MonsterAtt);
-    //    Damage(PlayerHp, MonsterAtt);
-
-    //    _getch();
-
-    //    //if (조건 만족시)
-    //    //{
-    //    //    return;
-    //    //}
-
-
-    //}
 }
